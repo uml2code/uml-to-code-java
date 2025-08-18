8ff6e05c1d95baf2df916ce2a816d4dc6048ea9d
walidmadad <walid.madad123@gmail.com>
2025-08-18 23:57:43 +0200

    Add UmlMethod Model #1

---
+++ backend/src/main/java/com/uml2code/backend/model/classes/UmlMethod.java
@@ -0,0 +1,20 @@
+package com.uml2code.backend.model.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UmlMethod {
+    private String name;
+    private String returnType;
+    /// TODO: class UmlParameter and Visibility (ENUM)
+    private Visibility visibility;
+    private List<UmlParameter> parameters;
+    private boolean isAbstract;
+    private boolean isStatic;
+}
