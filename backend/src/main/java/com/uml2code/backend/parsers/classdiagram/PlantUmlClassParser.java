fb6f5b5b9e757d08e242aba06e9f9438baf6d6d1
Walid Madad <77643737+walidmadad@users.noreply.github.com>
2025-08-20 12:34:03 +0000

    add TODO PlantUML parser for class diagrams

---
+++ backend/src/main/java/com/uml2code/backend/parsers/classdiagram/PlantUmlClassParser.java
@@ -0,0 +1,10 @@
+package com.uml2code.backend.parsers.classdiagram;
+
+import com.uml2code.backend.parsers.UmlParser;
+
+public class PlantUmlClassParser implements UmlParser{
+    /// TODO: Implement a PlantUML parser for class diagrams
+    public List<UmlClass> parse(File file) throws Exception {
+        return null;
+    }
+}
