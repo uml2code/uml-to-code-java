740ef287078225ab726aac382fa1efc6143c9c69
walidmadad <walid.madad123@gmail.com>
2025-08-21 00:56:29 +0200

    add check if is abstract, class name and visibility

--- backend/src/main/java/com/uml2code/backend/parsers/classdiagram/PlantUmlClassParser.java
+++ backend/src/main/java/com/uml2code/backend/parsers/classdiagram/PlantUmlClassParser.java
@@ -1,25 +1,27 @@
 package com.uml2code.backend.parsers.classdiagram;
 
 import com.uml2code.backend.model.classdiagram.UmlClass;
-import com.uml2code.backend.model.classdiagram.Visibility;
 import com.uml2code.backend.parsers.UmlParser;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.util.List;
 
 public class PlantUmlClassParser implements UmlParser{
     /// TODO: Implement a PlantUML parser for class diagrams
     @Override
     public List<UmlClass> parse(File file) throws Exception {
-        return null;
-    }
+        List<String> lines = Files.readAllLines(file.toPath());
+        UmlClass umlClass = null;
 
-    private Visibility parseVisibility(char visibility){
-        return switch (visibility){
-            case '+' -> Visibility.PUBLIC;
-            case '-' -> Visibility.PRIVATE;
-            case '#' -> Visibility.PROTECTED;
-            default -> Visibility.PACKAGE_PRIVATE;
-        };
+        for(String line : lines){
+           line = line.trim();
+           umlClass.setAbstract(Helpers.isAbstract(line));
+           umlClass.setVisibility(Helpers.parseVisibility(line.charAt(0)));
+           if(Helpers.isClassDefinition(line)){
+               umlClass.setName(Helpers.getClassName(line));
+           }
+        }
+        return null;
     }
 }
