8117b8bbbbd1e7d70015dff30f94a8ab86e6cbef
walidmadad <walid.madad123@gmail.com>
2025-08-21 00:49:01 +0200

    add Helpers

---
+++ backend/src/main/java/com/uml2code/backend/parsers/classdiagram/Helpers.java
@@ -0,0 +1,28 @@
+package com.uml2code.backend.parsers.classdiagram;
+
+import com.uml2code.backend.model.classdiagram.Visibility;
+
+public class Helpers {
+
+    protected static boolean isClassDefinition(String line){
+        return line.startsWith("class ");
+    }
+
+    protected static String getClassName(String line){
+        String[] parts = line.split(" ");;
+        return parts[parts.length - 1];
+    }
+
+    protected static boolean isAbstract(String line){
+        return line.contains("abstract");
+    }
+
+    protected static Visibility parseVisibility(char visibility){
+        return switch (visibility){
+            case '+' -> Visibility.PUBLIC;
+            case '-' -> Visibility.PRIVATE;
+            case '#' -> Visibility.PROTECTED;
+            default -> Visibility.PACKAGE_PRIVATE;
+        };
+    }
+}
