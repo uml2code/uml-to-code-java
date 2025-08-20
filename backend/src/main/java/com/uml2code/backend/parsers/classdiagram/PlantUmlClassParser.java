3c3e81eb438d6d82efdd767d435db33b5152c274
walidmadad <walid.madad123@gmail.com>
2025-08-21 01:04:13 +0200

    update logic

--- backend/src/main/java/com/uml2code/backend/parsers/classdiagram/PlantUmlClassParser.java
+++ backend/src/main/java/com/uml2code/backend/parsers/classdiagram/PlantUmlClassParser.java
@@ -5,6 +5,7 @@ import com.uml2code.backend.parsers.UmlParser;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlantUmlClassParser implements UmlParser{
@@ -12,17 +13,19 @@ public class PlantUmlClassParser implements UmlParser{
     @Override
     public List<UmlClass> parse(File file) throws Exception {
         List<String> lines = Files.readAllLines(file.toPath());
-        List<UmlClass> classes;
-        UmlClass umlClass = new UmlClass();
+        List<UmlClass> classes = new ArrayList<>();
 
         for(String line : lines){
-           line = line.trim();
-           umlClass.setAbstract(Helpers.isAbstract(line));
-           umlClass.setVisibility(Helpers.parseVisibility(line.charAt(0)));
+            line = line.trim();
            if(Helpers.isClassDefinition(line)){
+               UmlClass umlClass = new UmlClass();
+               umlClass.setAbstract(Helpers.isAbstract(line));
+               umlClass.setVisibility(Helpers.parseVisibility(line.charAt(0)));
                umlClass.setName(Helpers.getClassName(line));
+               classes.add(umlClass);
            }
+
         }
-        return null;
+        return classes;
     }
 }
