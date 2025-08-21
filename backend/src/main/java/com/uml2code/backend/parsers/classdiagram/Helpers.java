01141e058bc3e959a5ea268bca55b49fda7c41e1
Walid Madad <77643737+walidmadad@users.noreply.github.com>
2025-08-21 10:01:49 +0200

    Add a function to get the name of a method Helpers.java

--- backend/src/main/java/com/uml2code/backend/parsers/classdiagram/Helpers.java
+++ backend/src/main/java/com/uml2code/backend/parsers/classdiagram/Helpers.java
@@ -4,12 +4,13 @@ import com.uml2code.backend.model.classdiagram.Visibility;
 
 public class Helpers {
 
+    // Class Helpers
     protected static boolean isClassDefinition(String line){
         return line.startsWith("class ");
     }
 
     protected static String getClassName(String line){
-        String[] parts = line.split(" ");;
+        String[] parts = line.split(" ");
         return parts[parts.length - 1];
     }
 
@@ -17,12 +18,24 @@ public class Helpers {
         return line.contains("abstract");
     }
 
+    protected static boolean isClassEnd(String line){
+        return line.contains("}";
+    }
+
+    // Method Helpers
     protected static boolean isMethod(String line){
         return line.contains("(");
     }
 
-    protected static boolean isClassEnd(String line){
-        return line.contains("}";
+    protected static String getMethodName(String line){
+        String[] parts = line.split(" ");
+        String name = "";
+        for(String part: parts){
+            if(part.contains(":")){
+                name = part.substring(0, part.indexOf(":");
+            }        
+        }
+        return name;
     }
 
     protected static Visibility parseVisibility(char visibility){
