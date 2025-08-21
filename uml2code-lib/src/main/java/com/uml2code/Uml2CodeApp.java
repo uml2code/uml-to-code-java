626a0432abecf78b1240affa25ab75a5ca683062
walidmadad <walid.madad123@gmail.com>
2025-08-21 20:46:55 +0200

    remove spring boot

--- uml2code-lib/src/main/java/com/uml2code/Uml2CodeApp.java
+++ uml2code-lib/src/main/java/com/uml2code/Uml2CodeApp.java
@@ -16,13 +16,10 @@
 
 package com.uml2code;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackendApplication {
+public class Uml2CodeApp {
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        System.out.println("UML2Code Library Ready");
     }
 }
     
