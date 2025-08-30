68b12acfff514ae507dc80654d7f928f7d64d7a8
walidmadad <walid.madad123@gmail.com>
2025-08-30 20:24:19 +0200

    Add Copyright

--- src/main/java/com/uml2code/generators/javaGenerator/classDiagrammGenerator/JavaClassGenerator.java
+++ src/main/java/com/uml2code/generators/javaGenerator/classDiagrammGenerator/JavaClassGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Walid Madad and uml2code Organization
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.uml2code.generators.javaGenerator.classDiagrammGenerator;
 
 import com.uml2code.generators.CodeGenerator;
